bf9ad7f3c1b0ded874ae3c17ea5335066f7d5d35
Roky3029 <mikirb30@gmail.com>
2026-04-29 23:32:33 +0200

    fixed a bug with server/client components

--- src/methods/misc/getFollowing.ts
+++ src/methods/misc/getFollowing.ts
@@ -1,12 +1,12 @@
 'use server'
 
-import connectDB from '@/lib/connectDB'
 import User from '@/models/User'
 import { User as UserType } from '@/types/user'
 
 export const getFollowing = async (id: string) => {
-	await connectDB()
 	const userData: UserType = (await User.findById(id)) as UserType
+
+	if (!userData) return
 
 	const usersFollowing = await Promise.all(
 		userData.following.map(u => {
